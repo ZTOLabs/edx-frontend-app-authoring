1af3401d505a7a9f20f6451cff4a250419c4d956
Hieu Nguyen <thanhhieunguyen183@gmail.com>
2025-09-08 13:40:19 +0700

    feat: Setup tailwind, font family

--- src/index.jsx
+++ src/index.jsx
@@ -16,6 +16,11 @@ import { initializeHotjar } from '@edx/frontend-enterprise-hotjar';
 import { logError } from '@edx/frontend-platform/logging';
 import messages from './i18n';
 
+import '@fontsource/plus-jakarta-sans/400.css';
+import '@fontsource/plus-jakarta-sans/500.css';
+import '@fontsource/plus-jakarta-sans/600.css';
+import '@fontsource/plus-jakarta-sans/700.css';
+
 import {
   ComponentPicker,
   CreateLibrary,
