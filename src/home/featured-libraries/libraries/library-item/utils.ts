ace2b04b243799eeb21257ddc2f7a23280930992
Hieu Nguyen <thanhhieunguyen183@gmail.com>
2025-09-22 11:29:36 +0700

    chore: Remove unused library item

--- src/home/featured-libraries/libraries/library-item/utils.ts
+++
@@ -1,6 +0,0 @@
-/**
- * Removes leading and trailing slashes from a string.
- * @param {string} str - The string to trim.
- * @returns {string} The trimmed string.
- */
-export const trimSlashes = (str: string): string => str.replace(/^\/|\/$/g, '');
