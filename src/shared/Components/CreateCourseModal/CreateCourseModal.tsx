ec39daa21942f02c67b8ddd895e361ac1686e4cb
Hieu Nguyen <thanhhieunguyen183@gmail.com>
2025-09-24 17:30:14 +0700

    feat: Implement Create Course Modal

---
+++ src/shared/Components/CreateCourseModal/CreateCourseModal.tsx
@@ -0,0 +1,72 @@
+import { cn } from 'shared/lib/utils';
+import animationData from 'assets/lotties/steamese-bot.json';
+import Lottie from 'lottie-react';
+import { useDialog } from 'shared/context/dialog';
+import { useIntl } from '@edx/frontend-platform/i18n';
+import Button from '../Common/Button';
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from '../ui/dialog';
+import ThinkingProgress from './ThinkingProgress';
+import messages from './message';
+
+const CreateCourseModal = () => {
+  const { isOpen, close } = useDialog();
+  const intl = useIntl();
+
+  return (
+    <Dialog open={isOpen} onOpenChange={close}>
+      <DialogContent
+        className={cn(
+          'sm:max-w-[425px] tw-bg-white !tw-rounded-2xl !tw-w-[600px] !tw-max-w-[600px] tw-gap-0 !tw-p-0',
+          'tw-border tw-border-gray-200 tw-border-solid',
+          'tw-shadow-[0px_4px_6px_-2px_#10182808,0px_12px_16px_-4px_#10182814]',
+          'tw-overflow-hidden',
+        )}
+      >
+        <div className="tw-h-full tw-px-6">
+          <DialogHeader className="!tw-pt-8 !tw-pb-4">
+            <DialogTitle className="!tw-text-2xl !tw-font-semibold tw-text-gray-900">
+              {intl.formatMessage(messages.title)}
+            </DialogTitle>
+          </DialogHeader>
+          <div className="tw-pt-4 tw-pb-6 tw-flex tw-flex-col tw-gap-6">
+            <div
+              id="bot-container"
+              className="tw-w-full tw-h-[192px] tw-rounded-2xl tw-bg-gradient-to-tl tw-from-[#F5C8F5] tw-relative
+            tw-to-[#DADDFA]"
+            >
+              <Lottie
+                className="tw-absolute tw-top-0 tw-left-0 tw-w-full tw-h-full"
+                style={{
+                  transform: 'scale(1.5)',
+                }}
+                animationData={animationData}
+                loop
+                autoPlay
+              />
+            </div>
+            <DialogDescription className="tw-text-gray-600 tw-font-normal tw-text-md tw-text-center">
+              {intl.formatMessage(messages.description)}
+            </DialogDescription>
+            <ThinkingProgress />
+          </div>
+        </div>
+        <DialogFooter className="tw-sticky tw-bottom-0 tw-bg-gray-50 tw-p-6 !tw-w-full tw-flex !tw-gap-3 tw-flex-row !tw-justify-between">
+          <DialogClose asChild>
+            <Button variant="tertiary" size="sm" labels={{ default: intl.formatMessage(messages.goToCourseListButton) }} />
+          </DialogClose>
+          <Button variant="brand" size="sm" labels={{ default: intl.formatMessage(messages.goToCourseContentButton) }} />
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default CreateCourseModal;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    darkMode: ['class'],
-    prefix: 'tw-',
+  darkMode: ['class'],
+  prefix: 'tw-',
   content: ['./src/**/*.{js,jsx,ts,tsx}', './plugins/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   theme: {
   	extend: {
@@ -508,6 +508,28 @@ module.exports = {
   				border: 'hsl(var(--sidebar-border))',
   				ring: 'hsl(var(--sidebar-ring))'
   			}
+  		},
+  		keyframes: {
+  			'accordion-down': {
+  				from: {
+  					height: '0'
+  				},
+  				to: {
+  					height: 'var(--radix-accordion-content-height)'
+  				}
+  			},
+  			'accordion-up': {
+  				from: {
+  					height: 'var(--radix-accordion-content-height)'
+  				},
+  				to: {
+  					height: '0'
+  				}
+  			}
+  		},
+  		animation: {
+  			'accordion-down': 'accordion-down 0.2s ease-out',
+  			'accordion-up': 'accordion-up 0.2s ease-out'
   		}
   	}
   },
