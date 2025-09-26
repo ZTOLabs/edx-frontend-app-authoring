9bf78218b44de652ca33181b16b7d05ecc9e9729
ninh-nd <ninh.nd@outlook.com>
2025-09-26 13:59:30 +0700

    fix: revert mistake commit of eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,7 +21,6 @@ module.exports = createConfig('eslint', {
     'arrow-body-style': 'off',
     'object-curly-newline': 'off',
     'react/function-component-definition': 'off',
-    'react/jsx-filename-extension': [2, { extensions: ['.js', '.jsx', '.ts', '.tsx'] }],
   },
   settings: {
     // Import URLs should be resolved using aliases
